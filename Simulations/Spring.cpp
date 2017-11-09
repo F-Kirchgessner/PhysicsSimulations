e0d9fffc92164421e39473066a81f956a1f0280e
Thomas <a-t-e@gmx.de>
2017-11-09 12:07:15 +0100

    +Spring class added getter and setter

--- Simulations/Spring.cpp
+++ Simulations/Spring.cpp
@@ -14,3 +14,32 @@ Spring::Spring(int point1, int point2, float stiffness, float initialLength) : p
 Spring::~Spring()
 {
 }
+
+void Spring::setPoint1(int point) {
+	point1 = point;
+}
+void Spring::setPoint2(int point) {
+	point2 = point;
+}
+void Spring::setStiffness(float stiff) {
+	stiffness = stiff;
+}
+void Spring::setInitalLength(float initlength) {
+	initialLength = initlength;
+}
+
+int Spring::getPoint1() {
+	return point1;
+}
+
+int Spring::getPoint2() {
+	return point2;
+}
+
+float Spring::getStiffness() {
+	return stiffness;
+}
+
+float Spring::getInitialLength() {
+	return initialLength;
+}
