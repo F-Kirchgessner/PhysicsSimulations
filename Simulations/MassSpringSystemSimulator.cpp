f7ea113a82cbc874922431c1b7cd951b570c6f45
F-Kirchgessner <florian.kirchgessner@tum.de>
2017-11-09 11:46:52 +0100

    Added MassSpringSystemSimulator.cpp

--- Simulations/MassSpringSystemSimulator.cpp
+++ Simulations/MassSpringSystemSimulator.cpp
@@ -92,3 +92,32 @@ void MassSpringSystemSimulator::onMouse(int x, int y)
 	m_trackmouse.x = x;
 	m_trackmouse.y = y;
 }
+
+void MassSpringSystemSimulator::setMass(float mass) {}
+
+
+void MassSpringSystemSimulator::setStiffness(float stiffness) {}
+
+
+void MassSpringSystemSimulator::setDampingFactor(float damping) {}
+
+
+int MassSpringSystemSimulator::addMassPoint(Vec3 position, Vec3 Velocity, bool isFixed) {}
+
+
+void MassSpringSystemSimulator::addSpring(int masspoint1, int masspoint2, float initialLength) {}
+
+
+int MassSpringSystemSimulator::getNumberOfMassPoints() {}
+
+
+int MassSpringSystemSimulator::getNumberOfSprings() {}
+
+
+Vec3 MassSpringSystemSimulator::getPositionOfMassPoint(int index) {}
+
+
+Vec3 MassSpringSystemSimulator::getVelocityOfMassPoint(int index) {}
+
+
+void MassSpringSystemSimulator::applyExternalForce(Vec3 force) {}
